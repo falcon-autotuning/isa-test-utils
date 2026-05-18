07c7546877dc5d39645f1af31971e3d31e10aa39
tjk144 <tkovach@wisc.edu>
2026-05-18 11:43:59 -0500

    Fixed the script to use valid C names for the files

--- cmake/embed_bundle.cmake
+++ cmake/embed_bundle.cmake
@@ -9,46 +9,14 @@ function(embed_bundle)
   endif()
 
   file(WRITE ${EMBED_OUTPUT}
-  "#pragma once\n"
-  "#include <glib.h>\n"
-  "#include <isa-test-utils/setup.h>\n\n"
-  )
+"#pragma once
+#include <glib.h>
+#include <isa-test-utils/setup.h>
 
-  # ================================
-  # Helper macro: embed files
-  # ================================
-  macro(EMBED_CATEGORY FILE_LIST CATEGORY_NAME)
-    foreach(f ${${FILE_LIST}})
-      file(RELATIVE_PATH rel "${CMAKE_CURRENT_SOURCE_DIR}" "${f}")
-
-      string(REPLACE "/" "_" sym ${rel})
-      string(REPLACE "." "_" sym ${sym})
-
-      set(tmp "${CMAKE_CURRENT_BINARY_DIR}/${sym}.h")
-
-      execute_process(COMMAND xxd -i ${f} ${tmp})
-
-      file(READ ${tmp} content)
-
-      file(APPEND ${EMBED_OUTPUT} "${content}\n")
-
-      file(APPEND ${EMBED_OUTPUT} "
-  {
-    EmbeddedFile *e = g_new0(EmbeddedFile, 1);
-    e->relative_path = \"${rel}\";
-    e->data = ${sym};
-    e->size = ${sym}_len;
-    g_ptr_array_add(bundle->${CATEGORY_NAME}, e);
-  }
 ")
-    endforeach()
-  endmacro()
 
-  # ================================
-  # Generator function
-  # ================================
-  file(APPEND ${EMBED_OUTPUT} "
-static inline EmbeddedBundle get_embedded_bundle(void) {
+  file(APPEND ${EMBED_OUTPUT}
+"static inline EmbeddedBundle get_embedded_bundle(void) {
   EmbeddedBundle bundle = {0};
 
   bundle.isa_files = g_ptr_array_new();
@@ -56,12 +24,41 @@ static inline EmbeddedBundle get_embedded_bundle(void) {
   bundle.plugin_files = g_ptr_array_new();
 ")
 
+  macro(EMBED_CATEGORY FILE_LIST CATEGORY_NAME)
+    foreach(f ${${FILE_LIST}})
+      file(RELATIVE_PATH rel "${CMAKE_CURRENT_SOURCE_DIR}" "${f}")
+      string(REGEX REPLACE "[^A-Za-z0-9_]" "_" sym "${rel}")
+      string(TOLOWER "${sym}" sym)
+      set(tmp "${CMAKE_CURRENT_BINARY_DIR}/embed_${sym}.h")
+      execute_process(
+        COMMAND xxd -i -n ${sym} "${f}"
+        OUTPUT_FILE "${tmp}"
+        RESULT_VARIABLE rc
+      )
+      if(NOT rc EQUAL 0)
+        message(FATAL_ERROR "xxd failed for ${f}")
+      endif()
+      file(READ "${tmp}" content)
+      file(APPEND ${EMBED_OUTPUT} "${content}\n")
+      file(APPEND ${EMBED_OUTPUT}
+"  {
+    EmbeddedFile *e = g_new0(EmbeddedFile, 1);
+    e->relative_path = \"${rel}\";
+    e->data = ${sym};
+    e->size = ${sym}_len;
+    g_ptr_array_add(bundle.${CATEGORY_NAME}, e);
+  }
+")
+
+    endforeach()
+  endmacro()
+
   EMBED_CATEGORY(EMBED_ISA_FILES "isa_files")
   EMBED_CATEGORY(EMBED_CONFIG_FILES "config_files")
   EMBED_CATEGORY(EMBED_PLUGIN_FILES "plugin_files")
 
-  file(APPEND ${EMBED_OUTPUT} "
-  return bundle;
+  file(APPEND ${EMBED_OUTPUT}
+"  return bundle;
 }
 ")
 endfunction()
